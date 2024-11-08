05813e4db9a45ebdb8870e5c142c11d9d5d28b1d
Fred Morrison <frederick.morrison@outlook.com>
2024-11-08 16:23:27 -0700

    add JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,18 @@ public class Main {
         createAndShowGUI(scaledSize, title);
     }
 
+    /** Create and run the Swing compatible GUI
+     * @param scaledSize - the Dimension (width x height) of the JFrame
+     * @param title - String containing the title to be used by the JFrame
+     */
     private static void createAndShowGUI(Dimension scaledSize, String title) {
         SnowManGUI gui = new SnowManGUI(scaledSize, title);
         javax.swing.SwingUtilities.invokeLater(gui);
     }
 
+    /** Get information about the version of Java used to run this program.
+     * @return String containing information about the version of Java used to run this program
+     */
     private static String getJavaVersion() {
         Runtime.Version runTimeVersion = Runtime.version();
         return String.format("%s.%s.%s.%s", runTimeVersion.feature(), runTimeVersion.interim(), runTimeVersion.update(), runTimeVersion.patch());
